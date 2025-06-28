fe3a8bb42cc1fe1b3924f2dc45c1f4b9557f980c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-06-28 13:40:23 -0300

    feat: create route handler

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import http from "node:http";
 
 import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 function listener(request, response) {
     jsonHandler(request, response);
-    console.log(request.body);
+    routeHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);
