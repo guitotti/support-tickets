19a54a39af5cb35f6884f2470ff8e4d736086e56
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-06-28 00:09:47 -0300

    feat: create json handler

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import http from "node:http";
 
-function listener(request, response) {}
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+function listener(request, response) {
+    jsonHandler(request, response);
+    console.log(request.body);
+}
 
 http.createServer(listener).listen(3333);
